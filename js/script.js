9434c47caefbb096c3d8d57450e3e0fbde5b6721
Rebeckah McDaniel <rebeckah.mcdaniel@gmail.com>
2022-03-16 09:19:57 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Attach GIF's to respective albums
+
+
+
+// Make GIF's clickable on each album in center
+
+
+
+// Make song name load up when GIF starts
+
+
+
+// Possibly add a short audio clip to each album when GIF starts
