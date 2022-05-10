864782700811c8914eefd6275540f510193893f9
Crafty Becky <rebeckah.mcdaniel@gmail.com>
2022-05-10 15:52:51 -0600

    Added videos, started JS functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-// Attach GIF's to respective albums
+// Attach mp4's to respective albums
+
+
+// Make mp4's clickable on each album in center
 
 
 
-// Make GIF's clickable on each album in center
+// Make song name load up when mp4 starts
 
 
 
-// Make song name load up when GIF starts
+// Possibly add a short audio clip to each album when mp4 start
+const myVideo = document.getElementsByClassName('myVid');
+const btn = document.getElementsByClassName('clickBtn')
 
-
-
-// Possibly add a short audio clip to each album when GIF starts
+btn.addEventListener("click", function() {
+  myVid.play() = "uhf" ;
+  myVid.play() = "three-D" ;
+})
