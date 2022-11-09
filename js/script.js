dc32befe6e58913d3c55146990919e68f58ed4f9
Crafty Becky <rebeckah.mcdaniel@gmail.com>
2022-11-09 09:38:45 -0700

    JavaScript changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
-// Attach mp4's to respective albums
+//Play video on click of the album button
 
 
-// Make mp4's clickable on each album in center
+// var hideVid = document.getElementsByClassName('hideVideo')[0];
+//     hideVid.style.display = 'none';
+
+
+
+//reply_click not defined in code ** Need to fix! ** - Use a for...of 
+function reply_click(clicked_id) {
+    var hideVid = document.getElementsByClassName('hideVideo');
+    var vidNohighlight = document.getElementById('vidNohighlight');
+    
+    for ( var i = 0; i < hideVid.length - 1; i++ ) {
+        if (hideVid[i].id == 'video' + clicked_id) {
+            var isVisible = hideVid[i].style.display;
+            hideVid[i].style.display = isVisible === 'none' ? 'block' : 'none';
+            vidNohighlight.style.display = isVisible === 'none' ? 'visible' : 'none';
+        } else {
+            hideVid[i].style.display = 'none'; 
+        }
+    }
+}
+
+var vid = document.querySelector('video');
+var btn = document.querySelector('.button');
+
+
+    btn.addEventListener('click', function() {
+        vid.play();
+    });
+
+
+
+
+    
+
+
+//Hide all other videos until play on click
 
 
 
@@ -10,10 +45,4 @@
 
 
 // Possibly add a short audio clip to each album when mp4 start
-const myVideo = document.getElementsByClassName('myVid');
-const btn = document.getElementsByClassName('clickBtn')
 
-btn.addEventListener("click", function() {
-  myVid.play() = "uhf" ;
-  myVid.play() = "three-D" ;
-})
